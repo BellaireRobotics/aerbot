9125b3396d25c0bf4865f75376a58187afb0a1f4
LoadingPleaseWait <anonymous9304@gmail.com>
2014-04-04 15:43:37 -0500

    merge conflict solved

--- src/com/bellaire/aerbot/input/JoystickInput.java
+++ src/com/bellaire/aerbot/input/JoystickInput.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.bellaire.aerbot.input;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -40,80 +39,17 @@ public class JoystickInput implements InputMethod {
         return left.getRawButton(3);
     }
 
-    public boolean getShoot() {
-        return right.getTrigger();
-    }
-
     public boolean gearSwitch() {
         return false;
     }
 
-    public boolean getPrepareToShoot() {
-        return false;
-    }
-
-    public boolean getLeftTurn() {
-        return false;
-    }
-
-    public boolean getRightTurn() {
-        return false;
-    }
-
-}
-=======
-package com.bellaire.aerbot.input;
-
-import edu.wpi.first.wpilibj.Joystick;
-
-public class JoystickInput implements InputMethod {
-
-    private Joystick left, right;
-
-    public JoystickInput() {
-        left = new Joystick(1);
-        right = new Joystick(2);
-    }
-
-    public double getLeftY() {
-        return left.getY();
-    }
-
-    public double getRightY() {
-        return right.getY();
-    }
-
-    public double getLeftX() {
-        return left.getY();
-    }
-
-    public double getRightX() {
-        return right.getY();
-    }
-
-    public boolean getIntakeIn() {
-        return right.getRawButton(4);
-    }
-
-    public boolean getIntakeOut() {
-        return right.getRawButton(2);
-    }
-
-    public boolean getIntakePneumatic() {
-        return left.getRawButton(3);
-    }
-
     public boolean getShoot() {
-        return right.getTrigger();
-    }
-
-    public boolean gearSwitch() {
-        return false;
-    }
-
-    public boolean getAntiShoot() {
-        return false;
-    }
+      return right.getTrigger();
+  }
+  
+  public boolean getPrepareToShoot() {
+      return false;
+  }
 
     public boolean getLeftTurn() {
         return false;
@@ -133,4 +69,3 @@ public class JoystickInput implements InputMethod {
 		}
 
 }
->>>>>>> gearshift
